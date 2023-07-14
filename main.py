03de56549c5307c41d334795b68b1d4523281457
Phan Nguyễn Thành Duy <duypnt23@gmail.com>
2023-07-14 13:53:25 +0700

    update

--- main.py
+++ main.py
@@ -23,12 +23,6 @@ bot.remove_command('help')
 async def ping(ctx):
     await ctx.send(f'Pong! {round(bot.latency * 1000)}ms')
 
-# Load Cogs
-for filename in os.listdir('./cogs'):
-    if filename.endswith('.py'):
-        bot.load_extension(f'cogs.{filename[:-3]}')
-
-# On Ready
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user.name}#{bot.user.discriminator}')
